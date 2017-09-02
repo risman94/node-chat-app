cdc2cbd0ff8d9f9a87e676fd5149ab7282d655fe
risman94 <kamarulrisman1994@gmail.com>
2017-09-02 09:20:07 +0800

    change

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ io.on('connection', (socket) => {
 
 	socket.on('createMessage', (message) => {
 		console.log('createMessage', message);
-		io.emit('createMessage', {
+		io.emit('newMessage', {
 			from: message.from,
 			text: message.text,
 			createdAt: new Date().getTime()
