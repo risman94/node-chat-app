fe054e6b37e389b01483f2df6b28556aece5b77c
risman94 <kamarulrisman1994@gmail.com>
2017-09-03 05:25:15 +0800

    css

--- server/server.js
+++ server/server.js
@@ -19,9 +19,10 @@ io.on('connection', (socket) => {
 
 	socket.broadcast.emit('newMessage', generateMessage('admin', 'new user joined'));
 
-	socket.on('createMessage', (message) => {
+	socket.on('createMessage', (message, callback) => {
 		console.log('createMessage', message);
 		io.emit('newMessage', generateMessage(message.from, message.text));
+		callback();
 	});
 
 	socket.on('createLocationMessage', (coords) => {
