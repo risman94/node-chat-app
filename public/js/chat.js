2312bc4f3ecacb70297347a2f3533f24a7b12679
risman94 <kamarulrisman1994@gmail.com>
2017-09-03 12:08:23 +0800

    add join page that submit to chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollToBottom () {
+	//selectors
+	var messages = jQuery('#messages');
+	var newMessage = messages.children('li:last-child');
+	//height
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+	if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+		messages.scrollTop(scrollHeight);
+	}
+}
+
 socket.on('connect', function () {
 	console.log('connected to server');
 });
@@ -17,10 +32,7 @@ socket.on('newMessage', function (message) {
 		createdAt: formattedTime
 	});
 	jQuery('#messages').append(html);
-	// var li = jQuery('<li></li>');
-	// li.text( message.from + ' ' + formattedTime + ' : ' + message.text );
-
-	// jQuery('#messages').append(li);
+	scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -32,12 +44,7 @@ socket.on('newLocationMessage', function (message) {
 		createdAt: formattedTime
 	});
 	jQuery('#messages').append(html);
-	// var li = jQuery('<li></li>');
-	// var a = jQuery('<a target="_blank"> my current location </a>');
-	// li.text(message.from + ' ' + formattedTime + ' : ');
-	// a.attr('href', message.url);
-	// li.append(a);
-	// jQuery('#messages').append(li);
+	scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
